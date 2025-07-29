ee2bff97647e8bbf33425e38c46e12b3aa385c43
Shishir <shishir16subedi@gmail.com>
2025-07-29 12:23:57 +0545

    Feat(project): Added CRUD on project and initialized entry folder with essentials

--- src/entry/entities/entry.entity.ts
+++ src/entry/entities/entry.entity.ts
@@ -1,1 +1,29 @@
-export class Entry {}
+import { Project } from "src/project/entities/project.entity";
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn, ManyToOne } from "typeorm";
+
+@Entity('entries')
+export class Entry {
+    @PrimaryGeneratedColumn("uuid")
+    id: string;
+
+    @Column({ unique: true })
+    name: string;
+
+    @Column({ type: 'text' })
+    imgUrl: string;
+
+    // @Column({ type: 'text' })
+    // project: string;
+
+    @ManyToOne(() => Project, (project) => project.entries, { eager: true })
+    project: Project;
+
+    @Column({ type: 'text' })
+    userId: string;
+
+    @CreateDateColumn()
+    createdAt: Date;
+
+    @UpdateDateColumn()
+    updatedAt: Date;
+}
--- src/project/project.module.ts
+++ src/project/project.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ProjectService } from './project.service';
 import { ProjectController } from './project.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Project } from './entities/project.entity';
+import { UsersModule } from 'src/user/user.module';
 
 @Module({
+  imports: [UsersModule, TypeOrmModule.forFeature([Project])],
   controllers: [ProjectController],
   providers: [ProjectService],
 })
